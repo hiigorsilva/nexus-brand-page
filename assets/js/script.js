5fa0dc8ddf4f77b3f5af8bbb5000a02053d1025e
Higor Silva <contato.higordeveloper@gmail.com>
2023-07-06 19:36:00 -0300

    :pencil: Adicionando popup no portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-AOS.init();
+// BIBLIOTECA DE ANIMAÇÃO
+AOS.init()
+
+// POPUP PORTFOLIO
+function openPopup(index) {
+  const popup = document.querySelector(".popup-wrapper")
+  const popupImage = document.querySelector("#project")
+  const projectUrl = [
+    "./assets/imagens/project-direito.jpg",
+    "./assets/imagens/project-clinica.jpg",
+    "./assets/imagens/project-medico.jpg",
+    "./assets/imagens/thumb.jpg",
+  ]
+
+  popupImage.src = projectUrl[index]
+  popup.style.display = "flex"
+}
+
+function closePopup() {
+  const popup = document.querySelector(".popup-wrapper")
+  popup.style.display = "none"
+}
