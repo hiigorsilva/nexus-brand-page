312a1a199d18773ebd5b7af44e90075a54dc27ea
Higor Silva <contato.higordeveloper@gmail.com>
2023-08-28 13:24:21 -0300

    :pencil: add inspect button lock

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,41 @@ document
   .addEventListener("click", function (event) {
     event.stopPropagation()
   })
+
+// BLOQUEAR BOTÃO DIREITO DO MOUSE
+if (document.addEventListener) {
+  document.addEventListener("contextmenu", function (e) {
+    e.preventDefault()
+    return false
+  })
+} else {
+  //Versões antigas do IE
+  document.attachEvent("oncontextmenu", function (e) {
+    e = e || window.event
+    e.returnValue = false
+    return false
+  })
+}
+// BLOQUEAR ATALHOS COM TECLADO (CTRL + U, CTRL + S)
+if (document.addEventListener) {
+  document.addEventListener("keydown", bloquearSource)
+} else {
+  //Versões antigas do IE
+  document.attachEvent("onkeydown", bloquearSource)
+}
+
+function bloquearSource(e) {
+  e = e || window.event
+  var code = e.which || e.keyCode
+  if (
+    e.ctrlKey &&
+    (code == 83 || code == 85) //83 = S, 85 = U
+  ) {
+    if (e.preventDefault) {
+      e.preventDefault()
+    } else {
+      e.returnValue = false
+    }
+    return false
+  }
+}
